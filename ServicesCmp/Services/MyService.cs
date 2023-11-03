a125ce2c9d35ff9c2e2ef7b5d6409f9f8b4f5788
Christophe MOMMER <christophe.mommer@live.fr>
2023-11-03 07:51:25 +0100

    Add project files.

---
+++ ServicesCmp/Services/MyService.cs
@@ -0,0 +1,17 @@
+﻿namespace ServicesCmp.Services;
+public class MyService : IDisposable
+{
+    private string component;
+    public MyService()
+    {
+        Console.WriteLine("MyService created");
+    }
+
+    public string GetData() => "Data";
+    public string SetComponent(string cmp) => component = cmp;
+
+    public void Dispose()
+    {
+        Console.WriteLine("MyService disposed. Component : " + component);
+    }
+}
